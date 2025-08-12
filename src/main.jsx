038dd5f3b145dac2f920a61f4a5896bfc9428043
Syed1708 <snahmed2017@gmail.com>
2025-08-12 04:21:42 +0200

    update personal protfolio

--- src/main.jsx
+++ src/main.jsx
@@ -5,24 +5,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router';
 import { router } from './router/router.jsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import AuthProvider from './contexts/AuthProvider.jsx';
 
 AOS.init();
 
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <RouterProvider router={router} />
-      <ToastContainer />
-      </AuthProvider>
-    </QueryClientProvider>
+   <RouterProvider router={router} />
   </React.StrictMode>,
 );
